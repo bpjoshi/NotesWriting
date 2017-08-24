6deca331b1e92325c11e05d8aebba60ca9777f5c
bprasad <bhagwati.prasad@soprasteria.com>
2017-08-24 13:52:41 +0530

    Spring MVC final Commit

--- src/main/java/com/bpjoshi/stickynotes/controllers/LoginController.java
+++ src/main/java/com/bpjoshi/stickynotes/controllers/LoginController.java
@@ -7,11 +7,24 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class LoginController {
-		@RequestMapping(value="/login")
+		@RequestMapping(value="/login1")
 		//@ResponseBody
 		public String sayHello(){
 			return "Hello World!";
 		}
 		//It will give 404 error. Spring is searching for a view with name Hello World
 		//We would like to return it as a text not as a view so add @ResponseBody
+		//@Response body return the String back to browser
+		/*@RequestMapping(value="/login")
+		public String loginLink(){
+			return "login";
+		}*/
+		
+		//if we just return login.. it will become circular reponse
+		//..so we will have to configure view resolver
+		@RequestMapping(value = "/login", method = RequestMethod.GET)
+		public String showLoginPage() {
+			System.out.println("In login");
+			return "login";
+		}
 }
